4c484ff867ad7a7ca41dc2b8ce45cc0ff83225ed
Marko Rus <marko.rus59@gmail.com>
2020-04-25 12:45:40 +0200

    Some reformatting.

--- src/main.py
+++ src/main.py
@@ -1,46 +1,43 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 from classifier_handcrafted_features.model import HandcraftedFeatures
 from classifier_majority.model import Majority
 
-import os
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+tmp1 = np.zeros((5, 3))
+tmp2 = np.zeros((5, 3))
+err_a = np.zeros((5, 3))
+err_f = np.zeros((5, 3))
+path = r'../results/'
 
-target = ['Book relevance', 'Type', 'CategoryBroad']
-tmp1 = np.zeros((5,3))
-tmp2 = np.zeros((5,3))
-err_a = np.zeros((5,3))
-err_f = np.zeros((5,3))
-path =  r'../Results/'
-
-if not os.path.isdir(path):
-  os.mkdir(path)
-
-for t, tt in enumerate(target):
-  label = path + tt 
-  if os.path.isdir(label):
-    file = np.loadtxt(label+'/results.csv')
-    tmp1[:,t] = file[:,0]
-    err_a[:,t] = file[:,1]
-    tmp2[:,t] = file[:,2]
-    err_f[:,t] = file[:,3]
-  else:
-    tmp1[0,t], err_a[0, t], tmp2[0,t], err_f[0,t ] = Majority(target= tt).cross_validate()
-    tmp1[1,t], err_a[1, t], tmp2[1,t], err_f[1,t ] = HandcraftedFeatures('Bayes', target= tt).cross_validate()
-    tmp1[2,t], err_a[2, t], tmp2[2,t], err_f[2,t ] = HandcraftedFeatures('RF', target= tt).cross_validate()
-    tmp1[3,t], err_a[3, t], tmp2[3,t], err_f[3,t ] = HandcraftedFeatures('SVM', target= tt).cross_validate()
-    tmp1[4,t], err_a[4, t], tmp2[4,t], err_f[4,t ] = HandcraftedFeatures('LR',target= tt, standardize=True).cross_validate() 
-    os.mkdir(label)
-    np.savetxt(label+'/results.csv', np.vstack((tmp1[:,t],err_a[:,t],tmp2[:,t],err_f[:,t])).T)
+for i, target in enumerate(['Book relevance', 'Type', 'CategoryBroad']):
+    label = path + target
+    if os.path.isdir(label):
+        file = np.loadtxt(label + '/results.csv')
+        tmp1[:, i] = file[:, 0]
+        err_a[:, i] = file[:, 1]
+        tmp2[:, i] = file[:, 2]
+        err_f[:, i] = file[:, 3]
+    else:
+        tmp1[0, i], err_a[0, i], tmp2[0, i], err_f[0, i] = Majority(target=target).cross_validate()
+        tmp1[1, i], err_a[1, i], tmp2[1, i], err_f[1, i] = HandcraftedFeatures('Bayes', target=target).cross_validate()
+        tmp1[2, i], err_a[2, i], tmp2[2, i], err_f[2, i] = HandcraftedFeatures('RF', target=target).cross_validate()
+        tmp1[3, i], err_a[3, i], tmp2[3, i], err_f[3, i] = HandcraftedFeatures('SVM', target=target).cross_validate()
+        tmp1[4, i], err_a[4, i], tmp2[4, i], err_f[4, i] = HandcraftedFeatures('LR', target=target,
+                                                                               standardize=True).cross_validate()
+        os.mkdir(label)
+        np.savetxt(label + '/results.csv', np.vstack((tmp1[:, i], err_a[:, i], tmp2[:, i], err_f[:, i])).T)
 
 indx = ['Majority', 'NB', 'RF', 'SVM', 'LR']
-df_acc = pd.DataFrame(tmp1, columns = ['Book relevance', 'Type', 'CategoryBroad'], index = indx)
-df_f1 = pd.DataFrame(tmp2, columns = ['Book relevance', 'Type', 'CategoryBroad'], index = indx)
+df_acc = pd.DataFrame(tmp1, columns=['Book relevance', 'Type', 'CategoryBroad'], index=indx)
+df_f1 = pd.DataFrame(tmp2, columns=['Book relevance', 'Type', 'CategoryBroad'], index=indx)
 
-
-df_acc.plot.barh(title = 'Accuracy', xerr = err_a.T, capsize = 3)
-plt.savefig(path+'accuracy_plot.pdf', format = 'pdf')
-df_f1.plot.barh(title = 'F1 score', xerr = err_f.T, capsize = 3)
-plt.savefig(path+'f1_plot.pdf', format = 'pdf')
+df_acc.plot.barh(title='Accuracy', xerr=err_a.T, capsize=3)
+plt.savefig(path + 'accuracy_plot.pdf', format='pdf')
+df_f1.plot.barh(title='F1 score', xerr=err_f.T, capsize=3)
+plt.savefig(path + 'f1_plot.pdf', format='pdf')
--- src/model_base.py
+++ src/model_base.py
@@ -44,21 +44,21 @@ class Model:
 
     def cross_validate(self):
         acc = []
-        f1 =[]
+        f1 = []
 
         for xtrain, ytrain, xtest, ytest in kfolds(self.imap_columns, self.target):
             self.fit(xtrain, ytrain)
             y_predicted = self.predict(xtest)
 
             acc.append(metrics.accuracy_score(ytest, y_predicted))
-            
+
             # if self.target == 'Book relevance':
             #   f1.append(metrics.f1_score(ytest, y_predicted, average= 'binary'))
             # else:
-            f1.append(metrics.f1_score(ytest, y_predicted, average = 'weighted'))
+            f1.append(metrics.f1_score(ytest, y_predicted, average='weighted'))
         print(self)
         print('Accuracy (+- STD): {:.2f} +- {:.3f}'.format(np.mean(acc), np.std(acc)))
         print('F1 score (+- STD): {:.2f} +- {:.3f}'.format(np.mean(f1), np.std(f1)))
         print()
-        
-        return (np.mean(acc), np.std(acc),np.mean(f1), np.std(f1))
+
+        return (np.mean(acc), np.std(acc), np.mean(f1), np.std(f1))
